d030005cc8ad21585cf33ab68e553d4704fa28fe
Armensch11 <60622581+Armensch11@users.noreply.github.com>
2020-02-19 18:54:56 +0400

    Add files via upload

--- perfect Number.js
+++ perfect Number.js
@@ -2,23 +2,23 @@
 
 //divisions of the number
 function divides_to (check_Num) {
-    arrayof_Divisions = [];
+    arrayof_Divisors = [];
     for (let i = 1, j = 0; i <= check_Num/2 ; i++) {
         if ( check_Num % i === 0) {
-            arrayof_Divisions.push(i);
+            arrayof_Divisors.push(i);
             j++;
         }
     }
-    return arrayof_Divisions;
+    return arrayof_Divisors;
 }
 //finding perfect numbers
 function perfects_inthe_range (lower,upper) {
    for (i = lower; i <= upper; i++) {
        let sum = 0;
-       let devisions = divides_to(i);
-       for ( let j = 0; j < devisions.length; j++) {
-           sum += devisions[j];
-           if (j === devisions.length-1 && sum === i) {
+       let divisors = divides_to(i);
+       for ( let j = 0; j < divisors.length; j++) {
+           sum += divisors[j];
+           if (j === divisors.length-1 && sum === i) {
                console.log (`${i} is perfect`);
            }
        }
